09fce89bad825e0be2effa7310a52b85639583a0
Ariel Flesler <aflesler@gmail.com>
2025-07-22 15:43:32 -0300

    A few fixes

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,59 @@
 import { FastMCP } from 'fastmcp'
 
-import cli from './cli'
+import cli from './cli.js'
 import env from './env.js'
 import logger from './logger.js'
 import pkg from './pkg.js'
 import tools from './tools.js'
 
-const server = new FastMCP({
-  name: `${pkg.author}/${pkg.name}`,
-  version: pkg.version,
-})
 
-// Register all tools & resources
-for (const tool of Object.values(tools)) {
-  if (!tool.isEnabled) {
-    continue
+async function start() {
+  const server = new FastMCP({
+    name: `${pkg.author}/${pkg.name}`,
+    version: pkg.version,
+  })
+  // Register all tools & resources
+  for (const tool of Object.values(tools)) {
+    if (!tool.isEnabled) {
+      continue
+    }
+    if (tool.isResource) {
+      // Register as resource
+      server.addResource({
+        uri: `resource://${tool.name}`,
+        name: tool.description,
+        mimeType: 'text/plain',
+        load: () => cli.runTool(tool, []).then(text => ({ text })),
+      })
+    } else {
+      // Register as tool with enhanced logging
+      server.addTool({
+        annotations: {
+          openWorldHint: false, // This tool doesn't interact with external systems
+          readOnlyHint: tool.isReadOnly,
+          title: tool.name,
+        },
+        name: tool.name,
+        description: tool.description,
+        parameters: tool.schema,
+        execute: (args) => cli.runTool(tool, args),
+      })
+    }
   }
-  if (tool.isResource) {
-    // Register as resource
-    server.addResource({
-      uri: `resource://${tool.name}`,
-      name: tool.description,
-      mimeType: 'text/plain',
-      load: () => cli.runTool(tool, []).then(text => ({ text })),
+
+  if (env.TRANSPORT === 'http') {
+    await server.start({
+      transportType: 'httpStream',
+      httpStream: {
+        port: env.PORT,
+      },
     })
   } else {
-    // Register as tool with enhanced logging
-    server.addTool({
-      annotations: {
-        openWorldHint: false, // This tool doesn't interact with external systems
-        readOnlyHint: tool.isReadOnly,
-        title: tool.name,
-      },
-      name: tool.name,
-      description: tool.description,
-      parameters: tool.schema,
-      execute: (args) => cli.runTool(tool, args),
+    await server.start({
+      transportType: 'stdio',
     })
+
+    logger.log('Started new server', { transport: env.TRANSPORT })
   }
 }
-
-if (env.TRANSPORT === 'http') {
-  await server.start({
-    transportType: 'httpStream',
-    httpStream: {
-      port: env.PORT,
-    },
-  })
-} else {
-  await server.start({
-    transportType: 'stdio',
-  })
-
-  logger.log('Started new server', { transport: env.TRANSPORT })
-}
-
-export default server
+export default { start }
